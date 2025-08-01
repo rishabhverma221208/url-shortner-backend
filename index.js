d6304f2c5f36f6672ae0e7f84db75c8701980062
Rishabh Verma <rishabhverma221208@gmail.com>
2025-08-02 00:50:30 +0530

    .

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const URL = require("./model/url");
 const staticRoute = require("./routes/staticRouter");
 
 const app = express();
-const port= process.env.PORT || 8000;
-
 const MONGODB_URL = process.env.MONGO_URL;
 
 connectToMongoDB(MONGODB_URL)
-  .then(() => console.log("Database connected successfully"));
+  .then(() => console.log("Database connected successfully"))
+  .catch(err => console.error("Mongo connection failed", err));
 
-app.set("view engine","ejs");
-app.set("views",path.resolve("./views"));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +22,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/url", urlRoute);
 app.use("/", staticRoute);
 
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-})
+module.exports = app;
